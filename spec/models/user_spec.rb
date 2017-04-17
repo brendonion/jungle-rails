58ae98ddb6e7681d6e3fa49b5344a87acd077e14
Brendan Walker <brenny_walker@hotmail.com>
2017-04-17 21:15:54 +0000

    Adds password authentication class method to sessions controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe User, type: :model do
         first_name: 'Brendan',
         last_name: 'Walker',
         email: 'chester@lester.com',
-        password_digest: 'password'
+        password: 'password',
+        password_confirmation: 'password'
       )
       expect(@user).to be_valid
     end
@@ -20,6 +21,7 @@ RSpec.describe User, type: :model do
         password_confirmation: 'lester'
       )
       expect(@user.password).to_not eq(@user.password_confirmation)
+      expect(@user).to_not be_valid
     end
     it 'user must have a unique email' do
       @user = User.create(
@@ -40,6 +42,15 @@ RSpec.describe User, type: :model do
       expect(@user.email).to eq(@user2.email)
       expect(@user2).to_not be_valid
     end
-    
+    it 'password must have minimum length of 6 characters' do
+      @user = User.create(
+        first_name: 'Brendan',
+        last_name: 'Walker',
+        email: 'chester@lester.com',
+        password: 'pass',
+        password_confirmation: 'pass'
+      )
+      expect(@user).to_not be_valid
+    end
   end
 end
